782dd2f624d6ad3d1bb7297d9abb6387ff461183
pma <pma@ubuntu1.myguest.virtualbox.org>
2023-12-31 21:03:00 +0200

    135 output a matrix

--- 152_Pescariu_MateiAlexandru_0.s
+++ 152_Pescariu_MateiAlexandru_0.s
@@ -3,7 +3,8 @@
 	m: .space 4
 	matrix: .space 404
 
-	cout: .asciz "%hu\n"
+	cout: .asciz "%hu "
+	nl: .asciz "\n"
 	
 .text
 
@@ -16,17 +17,55 @@
 		push 4(%esp)
 		push $cout
 		call printf
+		pushl $0
+		call fflush
+		add $12,%esp
+		ret
+		
+	printNewLine:
+		push $nl
+		call printf
+		pushl $0
+		call fflush
 		add $8,%esp
 		ret
 
 	printMatrix:
 		pushl %ebp
 		mov %esp, %ebp
-		push 
+		push $1 # lineIndex
+		push $1 # columnIndex
+		lea matrix, %esi # (comment)
 		
+		printMatrix__Line:
+			movl $1, -8(%ebp)
+			
+			printMatrix__Column:
+				movl -4(%ebp), %eax
+				mull n
+				addl -8(%ebp), %eax
+				
+				push (%esi, %eax, 4)
+				call printLong
+				addl $4, %esp
+				
+				incl -8(%ebp)
+				movl -8(%ebp), %eax
+				cmp m,%eax
+				jle printMatrix__Column
+				
+			call printNewLine
+			
+			incl -4(%ebp)
+			movl -4(%ebp), %eax
+			cmp n, %eax
+			jg printMatrix__Exit
+			jmp printMatrix__Line
 		
-		popl %ebp
-		ret
+		printMatrix__Exit:
+			addl $8,%esp
+			popl %ebp
+			ret
 
 
 
@@ -37,12 +76,8 @@
 	call readLong
 	pop %eax
 
-	push n
-	call printLong
-	pop %eax
-
 	pushl $m
-	call read
+	call readLong
 	popl %eax
 
 	call printMatrix
