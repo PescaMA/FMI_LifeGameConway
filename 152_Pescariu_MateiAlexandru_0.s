e1b975b45f263bc53d99730741307afdb15a962e
pma <pma@ubuntu1.myguest.virtualbox.org>
2024-01-01 11:51:23 +0200

    t250 (probably) functional matrix

--- 152_Pescariu_MateiAlexandru_0.s
+++ 152_Pescariu_MateiAlexandru_0.s
@@ -1,19 +1,26 @@
 .data
-	n: .space 4
-	m: .space 4
+	n: .space 4 # nr. of lines
+	m: .space 4 # nr. of columns
+	p: .space 4 # nr. of live cells
+	k: .space 4 # nr. of evolutions
+	# tmp: .space 4 # temporary variable
 	matrix: .space 404
 
-	cout: .asciz "%hu "
+	cin: .asciz "%ld"
+	cout: .asciz "%ld "
 	nl: .asciz "\n"
 	
 .text
 
 	readLong:
-		mov 4(%esp), %eax
-		movl $4, (%eax)
+		push 4(%esp)
+		push $cin
+		call scanf
+		addl $8, %esp
 		ret
 
 	printLong:
+		
 		push 4(%esp)
 		push $cout
 		call printf
@@ -23,6 +30,7 @@
 		ret
 		
 	printNewLine:
+		
 		push $nl
 		call printf
 		pushl $0
@@ -35,7 +43,7 @@
 		mov %esp, %ebp
 		push $1 # lineIndex
 		push $1 # columnIndex
-		lea matrix, %esi # (comment)
+		lea matrix, %esi
 		
 		printMatrix__Line:
 			movl $1, -8(%ebp)
@@ -52,14 +60,14 @@
 				incl -8(%ebp)
 				movl -8(%ebp), %eax
 				cmp m,%eax
-				jle printMatrix__Column
+				jl printMatrix__Column
 				
 			call printNewLine
 			
 			incl -4(%ebp)
 			movl -4(%ebp), %eax
 			cmp n, %eax
-			jg printMatrix__Exit
+			jge printMatrix__Exit
 			jmp printMatrix__Line
 		
 		printMatrix__Exit:
@@ -71,7 +79,7 @@
 
 .global main
 	main:
-
+	
 	push $n
 	call readLong
 	pop %eax
@@ -79,11 +87,56 @@
 	pushl $m
 	call readLong
 	popl %eax
+	
+	incl n # making space for the border.
+	incl m # making space for the border.
+	
+	pushl $p
+	call readLong
+	popl %eax
+	
+	push %ebp
+	mov %esp, %ebp
+	
+	push $0 # lineIndex
+	push $0 # columnIndex
+	xor %ecx,%ecx
+
+	loopReadLive:
+	
+		push %ecx
+		lea -4(%ebp), %ebx
+		push %ebx
+		call readLong
+		pop %ebx
+		incl -4(%ebp)
+		
+		lea -8(%ebp), %ebx
+		push %ebx
+		call readLong
+		pop %ebx
+		incl -8(%ebp)
+		
+		lea matrix, %edi
+		movl -4(%ebp), %eax
+		mull n
+		addl -8(%ebp), %eax
+		movl $1, (%edi, %eax, 4)
+		
+		pop %ecx
+		incl %ecx
+		cmp p, %ecx
+		jl loopReadLive
+		
+	addl $8, %esp
+	pop %ebp
+	
+	pushl $k
+	call readLong
+	popl %eax
+	
 
 	call printMatrix
-
-
-
 
 	exit:
 	mov $1, %eax
