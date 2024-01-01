381cfd13e85de79bf4653ff02af48a5627b6986f
pma <pma@ubuntu1.myguest.virtualbox.org>
2024-01-01 19:29:25 +0200

    t645 nr of neighbours

--- 152_Pescariu_MateiAlexandru_0.s
+++ 152_Pescariu_MateiAlexandru_0.s
@@ -65,14 +65,14 @@
 		pushl %ebp
 		mov %esp, %ebp
 		
-		
+		lea matrix, %esi
 		push $1 # lineIndex
 		push $1 # columnIndex
-		lea matrix, %esi # lea neighbours, %esi
 		
 		printMatrix__Line:
+		
+		
 			movl $1, -8(%ebp)
-			
 			printMatrix__Column:
 				movl -4(%ebp), %eax
 				mull maxN
@@ -87,22 +87,79 @@
 				incl -8(%ebp)
 				movl -8(%ebp), %eax
 				cmp m,%eax
-				jl printMatrix__Column
+				jle printMatrix__Column
 				
 			call printNewLine
 			
 			incl -4(%ebp)
 			movl -4(%ebp), %eax
 			cmp n, %eax
-			jge printMatrix__Exit
-			jmp printMatrix__Line
+			jle printMatrix__Line
 		
 		printMatrix__Exit:
 			addl $8,%esp
 			popl %ebp
 			ret
 
-
+	evolve:
+		
+		push %ebp
+		movl %esp, %ebp
+		call copyMatrix
+		
+		push $1 # lineIndex
+		push $1 # columnIndex
+		push $0 # nr. of live neighbours
+		lea matrix, %esi
+		lea matrixCopy, %edi
+		
+		evolve__Line:
+			movl $1, -8(%ebp)	
+			evolve__Column:
+				movl -4(%ebp), %eax
+				mull maxN
+				addl -8(%ebp), %eax
+				lea (%esi, %eax), %edx
+				
+				movl $0, -12(%ebp)
+				xor %ecx, %ecx
+				loopNeighbour:
+					
+					cmp $8,%ecx
+					jge evolve__Line_1
+					
+					lea neighbours, %eax
+					movl (%eax,%ecx,4), %ebx
+					
+					inc %ecx
+					cmpb $1, (%edx,%ebx)
+					
+					jne loopNeighbour
+					test:
+					incl -12(%ebp)
+					jmp loopNeighbour
+					
+					
+				evolve__Line_1:
+				
+					push -12(%ebp)
+					call printLong
+					popl %eax
+				
+				
+					incl -8(%ebp)
+					movl -8(%ebp), %eax
+					cmp m,%eax
+					jle evolve__Column
+			
+			call printNewLine
+			incl -4(%ebp)
+			movl -4(%ebp), %eax
+			cmp n, %eax
+			jle evolve__Line
+		addl $12,%esp
+		pop %ebp
+		ret
 
 
 
@@ -116,9 +173,6 @@
 	pushl $m
 	call readLong
 	popl %eax
-	
-	incl n # making space for the border.
-	incl m # making space for the border.
 	
 	pushl $p
 	call readLong
@@ -166,14 +220,22 @@
 	
 	# finished reading all input.
 	
-	call copyMatrix
-	
-	
-	
-	
-	call printMatrix
+	xor %ecx, %ecx
+	loopKvolution:
+		cmp k, %ecx
+		jge exit
+		push %ecx
+		
+		call evolve	
+		call printNewLine
+		
+		pop %ecx
+		inc %ecx
+		jmp loopKvolution
 
 	exit:
+	call printMatrix
+	
 	mov $1, %eax
 	xor %ebx, %ebx
 	int $0x80
